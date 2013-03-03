701d35bcefedca20f54059ae5093a9747b7d17b5
h3idi <heidi.harman@gmail.com>
2013-03-03 20:57:05 +0800

    created app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pennycloud::Application.config.secret_token = '95737f41ec545348c660725866a5f638377a0c6462b9a11cca335f72817fc9ed09f4df017c1f61cabc0398a32ab51c79af7e7a6bf1e7e7ff24bc2dae58a9e511'
