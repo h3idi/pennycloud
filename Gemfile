7bb5d7b60640abc580b075faf9514cc3f374b77d
h3idi <heidi.harman@gmail.com>
2013-03-04 20:24:19 +0800

    added css and signup form and link

--- Gemfile
+++ Gemfile
@@ -7,6 +7,8 @@ gem 'rails', '3.2.8'
 
 gem 'sqlite3'
 gem 'bootstrap-sass'
+gem 'devise'
+gem 'simple_form'
 
 
 # Gems used only for assets and not required
--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,6 @@
 require File.expand_path('../boot', __FILE__)
 
+
 require 'rails/all'
 
 if defined?(Bundler)
@@ -58,5 +59,7 @@ module Pennycloud
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    config.assets.initialize_on_precompile = false
   end
 end
